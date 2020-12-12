10e15bc40818268cfb6777cf14870d9545c76839
bored-user <joao.guimaraes.alves.vaz@gmail.com>
2020-12-12 12:33:36 -0300

    Performance improvements & better detection when execises finish

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ import time
 
 from pyppeteer import launch
 
-page = browser = None
+duolingo = browser = None
 cwd = os.path.dirname(os.path.realpath(__file__))
 language = json.load(open('languages/Norwegian.json', 'r'))
 
 
 async def wait_loading(query, page=None):
     if page == None:
-        page = globals()['page']
+        page = globals()['duolingo']
 
     element = await page.querySelector(query)
 
@@ -27,23 +27,24 @@ async def wait_loading(query, page=None):
 
 async def login(username, password):
     await (await wait_loading('a[data-test=have-account]')).click()
-    await (await page.querySelector("input[placeholder='Email or username']")).type(username, { 'delay': 200 })
-    await (await page.querySelector("input[placeholder='Password']")).type(password, { 'delay': 200 })
-    await (await page.querySelector('button[type=submit]')).click()
+    await (await duolingo.querySelector("input[placeholder='Email or username']")).type(username)
+    await (await duolingo.querySelector("input[placeholder='Password']")).type(password)
+    await (await duolingo.querySelector('button[type=submit]')).click()
+
     print('Logged in!')
 
 
 async def start_lesson():
-    await (await wait_loading('div[data-test=skill-icon]')).click()
-    await (await wait_loading('button[data-test=start-button]')).click()
-
+    await wait_loading('div[data-test=skill-icon]')
+    await duolingo.goto(language['url'])
     await wait_loading('button[data-test=quit-button]')
+
     print('Started lesson!\n')
 
 
 async def skip_exercise():
-    skip_btn = await page.querySelector('button[data-test=player-skip]')
-    can_skip = bool(await (page.evaluate("b => b.innerText === \"CAN'T LISTEN NOW\" || b.innerText === \"CAN'T SPEAK NOW\"", skip_btn))) if skip_btn != None else False
+    skip_btn = await duolingo.querySelector('button[data-test=player-skip]')
+    can_skip = bool(await (duolingo.evaluate("b => b.innerText === \"CAN'T LISTEN NOW\" || b.innerText === \"CAN'T SPEAK NOW\"", skip_btn))) if skip_btn != None else False
 
     if can_skip:
         await skip_btn.click()
@@ -52,64 +53,78 @@ async def skip_exercise():
     return can_skip
 
 
-async def translate(sentence, language):
-    browser = await launch()
-    page = await browser.newPage()
+async def translate(sentence, language, translator):
+    await translator.goto(f'https://translate.google.com/?sl=auto&tl={language}&text={sentence}&op=translate')
+    translation = await translator.evaluate('node => node.innerText', (await wait_loading('span[jsname=jqKxS]', translator)))
 
-    await page.goto(f'https://translate.google.com/?sl=auto&tl={language}&text={sentence}&op=translate')
-    translation = await page.evaluate('node => node.innerText', (await wait_loading('span[jsname=jqKxS]', page)))
-
-    await browser.close()
     return translation
 
 
-async def get_sentence(): return (await page.querySelectorAllEval('span[data-test=hint-sentence] > *', "words => encodeURIComponent(words.map(word => word.innerHTML).join(''))"))
+async def get_sentence(): return (await duolingo.querySelectorAllEval('span[data-test=hint-sentence] > *', "words => encodeURIComponent(words.map(word => word.innerHTML).join(''))"))
 
 
-async def solve_exercise():
-    await page.screenshot({'path': 'realtime.png'})
+async def solve_exercise(translator):
+    await duolingo.screenshot({'path': 'realtime.png'})
 
     try:
-        code = language['code'] if await page.querySelectorEval('h1[data-test=challenge-header] span', 'h => h.textContent') == 'Write this in Norwegian (Bokmål)' else 'en'
+        code = language['code'] if language['name'] in await duolingo.querySelectorEval('h1[data-test=challenge-header] span', 'h => h.textContent') else 'en'
         sentence = await get_sentence()
 
-        await (await page.querySelector('textarea[data-test=challenge-translate-input]')).type((await translate(sentence, code)) if not sentence in language['exceptions'] else language['exceptions'][sentence], { 'delay': 200 })
-        await next_exercise()
+        await (await duolingo.querySelector('textarea[data-test=challenge-translate-input]')).type((await translate(sentence, code, translator)) if not sentence in language['exceptions'] else language['exceptions'][sentence])
+        await click_next()
+
         print('Solved writting exercise!')
     except:
         pass
 
 
 async def check_mistake():
-    mistake = await page.querySelector("div[data-test='blame blame-incorrect']")
+    mistake = await duolingo.querySelector("div[data-test='blame blame-incorrect']")
     if mistake == None:
         return
 
     sentence = await get_sentence()
-    correct_form = await page.evaluate('m => m.children[1].children[0].children[0].children[1]', mistake)
+    correct_form = await duolingo.evaluate('m => m.children[1].children[0].children[0].children[1].textContent', mistake)
 
     language['exceptions'] = {} if not 'exceptions' in language else language['exceptions']
     language['exceptions'][sentence] = correct_form
-    print(f'Found an exception:\n    -> Input: {sentence}\n  -> Correct form: {correct_form}\nAlready added to exception list!')
+
+    print(f'Found an exception:\n    -> Input: {sentence}\n    -> Correct form: {correct_form}\nAlready added to exception list!')
 
 
-async def next_exercise():
+async def click_next():
     continue_btn = 'button[data-test=player-next]'
 
     await wait_loading(continue_btn)
-    await (await page.querySelector(continue_btn)).click()
+    await (await duolingo.querySelector(continue_btn)).click()
+
+
+async def tidy_up():
+    os.remove('realtime.png')
+    json.dump(language, open('languages/Norwegian.json', 'w'))
+
+    while duolingo.url != 'https://www.duolingo.com/learn':
+        await click_next()
+        time.sleep(0.1)
+
+    time.sleep(2)
+    await browser.close()
 
 
 async def main():
     print('Starting...')
+
     global browser
-    global page
+    global duolingo
 
     browser = await launch()
-    page = await browser.newPage()
+    duolingo = await browser.newPage()
+    translator = await browser.newPage()
 
-    await page.setViewport({'width': 1920, 'height': 948})
-    await page.goto('https://www.duolingo.com/?isLoggingIn=true')
+    await duolingo.setViewport({'width': 1920, 'height': 948})
+    await translator.setViewport({'width': 1920, 'height': 948})
+
+    await duolingo.goto('https://www.duolingo.com/')
 
     with open('auth/username') as username, open('auth/password') as password:
         await login(username.read(), password.read())
@@ -117,14 +132,12 @@ async def main():
     await start_lesson()
 
     while True:
-        await solve_exercise() if not await skip_exercise() else None
+        await solve_exercise(translator) if not await skip_exercise() else None
         await check_mistake()
-        await next_exercise()
+        await click_next()
 
-        if (((await page.querySelector('div[data-test=skill-tree]')) != None) and ((await page.querySelector('div[data-test=tree-section]')) != None) or (await page.querySelectorEval('button[data-test=quit-button]', "b => b.nextSibling.children[0].style.width == '100%'"))):
-            os.remove('realtime.png')
-            json.dump(language, open('languages/Norwegian.json', 'w'))
-            time.sleep(2)
+        if await duolingo.querySelectorEval('button[data-test=quit-button]', "b => b.nextSibling.children[0].style.width == '100%'"):
+            await tidy_up()
             break
 
         time.sleep(2)
@@ -132,5 +145,6 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.get_event_loop().run_until_complete(main())
+
     print('\nFinished! Bye!')
     exit()
